64dfbd3a599a24e1345f58f5cfbea82d63a2c45d
takahisa <s.takahisa_0903@docomo.ne.jp>
2019-04-06 06:49:23 +0000

    #2 add:ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
+  get '/home',   to: 'static_pages#home'
+  get '/about',   to: 'static_pages#about'
+  get '/question',   to: 'static_pages#question'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
