af9e4665022ac2e1d9229317b05ece590604a110
takahisa <s.takahisa_0903@docomo.ne.jp>
2019-04-10 08:44:16 +0000

    #8 update: 管理者権限の設定
    管理者権限が付与されているユーザーに管理画面へのアクセスと全てのモデルに対して全ての操作（新規保存、編集、削除など）できるように許可

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -2,14 +2,14 @@ RailsAdmin.config do |config|
 
   ### Popular gems integration
 
-  ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
+  # == Devise ==
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+  config.current_user_method(&:current_user)
 
-  ## == Cancan ==
-  # config.authorize_with :cancan
+  # == Cancan ==
+  config.authorize_with :cancan
 
   ## == Pundit ==
   # config.authorize_with :pundit
