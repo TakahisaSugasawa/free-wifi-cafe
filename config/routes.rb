32a01aa2623ad70c33cee40ad97a03771ef334b9
takahisa <s.takahisa_0903@docomo.ne.jp>
2019-04-13 00:46:47 +0000

    #13 update: ルーティングの追加
    resource :articlesを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   
+  resource :articles
+  
   root 'static_pages#home'
 
   get '/home',   to: 'static_pages#home'
