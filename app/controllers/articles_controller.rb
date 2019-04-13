95342d77c6ca98e36669fe9bab525e6d99dfa505
takahisa <s.takahisa_0903@docomo.ne.jp>
2019-04-13 03:56:19 +0000

    #13 update: コントローラー内で新規に登録するデータを保存する処理を追加
    permitメソッドと保存後の処理を追加追加
    EO<

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,15 @@ class ArticlesController < ApplicationController
   
   # get /articles
   def create
-        
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article
   end
+  
+  private
+    def article_params
+      params.require(:article).permit(:store_name, :adress, :access, :area,
+                              :station, :access, :wifi, :plug, :business_hours,
+                              :regular_holiday, :phone, :url)
+    end
 end
