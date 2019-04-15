30192efd3dfe5b83a5601275e778e382d86b301d
takahisa <s.takahisa_0903@docomo.ne.jp>
2019-04-15 10:58:03 +0000

    #13 fix: herokuにpushした時に起き る不具合の修正
    web_consoleを本番環境以外で使用するように設定

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,10 @@ Bundler.require(*Rails.groups)
 module FreeWifiCafe
   class Application < Rails::Application
     config.i18n.default_locale = :ja
-    config.web_console.whitelisted_ips = '106.171.53.249'
+    
+    unless Rails.env.production?
+      config.web_console.whitelisted_ips = '106.171.53.249'
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
