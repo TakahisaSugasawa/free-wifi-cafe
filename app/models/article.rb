c8edf9badae6b2c3e4c64da00b74c0c264b90feb
takahisa <s.takahisa_0903@docomo.ne.jp>
2019-04-17 12:20:55 +0000

    #14 :fix fリレーションの誤字を修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
-  has_many :commtents, dependent: :destroy
+  has_many :comments, dependent: :destroy
   validates :store_name, presence: true
 
 end
