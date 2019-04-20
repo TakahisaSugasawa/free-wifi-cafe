00de3b0cfd9babbcc002c7a02f21cada66ae445f
takahisa <s.takahisa_0903@docomo.ne.jp>
2019-04-20 00:39:12 +0000

    #14 update:　コメント生成
    commentsコントローラーにcreateアクショ

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+  
+  #POST /articles/:article_id/comments
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.build(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      # logger.debug @comment.errors.inspect
+      # logger.debug @comment.inspect
+      redirect_to article_path(@article)
+    else
+      render :"articles/new"
+    end
+    
+  end
+  
+  private
+    def comment_params
+      params.require(:comment).permit(:title,:content)
+    end
 end
