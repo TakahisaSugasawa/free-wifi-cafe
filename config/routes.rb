e2c2d9a3a81ea9c71350bebccc0c2f8eb2a28819
takahisa <s.takahisa_0903@docomo.ne.jp>
2019-04-21 11:38:47 +0000

    #14 update:ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   
   resources :articles
   
-  resources :articles do 
+  resources :articles , only: [:create , :destroy ] do 
     resources :comments
   end
   
