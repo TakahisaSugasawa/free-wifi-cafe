27f9fdb266cd7c6d8e4c77b7dbb7e6147d1962fa
takahisa <s.takahisa_0903@docomo.ne.jp>
2019-04-23 12:21:53 +0000

    #16:update:ストロングパラメータにimageカラムを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, 
-                                      keys: [:username,:self_introduction])
+                              keys: [:username,:self_introduction,:image])
   end
 end
