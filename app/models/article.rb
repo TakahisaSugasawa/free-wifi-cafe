18e62cbe099c0a80ff1adbae9a355c952e1a4c7d
takahisa <s.takahisa_0903@docomo.ne.jp>
2019-04-27 10:54:51 +0000

    #21 update: articlesモデルとアップローダーを

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,5 @@ class Article < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   validates :store_name, presence: true
-
+  mount_uploader :image, ImageUploader #Articleモデルとアップローダーを紐づける    
 end
