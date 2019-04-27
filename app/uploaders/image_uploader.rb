2800c8a26e52a07294f06fcd2848477086369b00
takahisa <s.takahisa_0903@docomo.ne.jp>
2019-04-27 04:00:11 +0000

    #16 update:サムネイルの高さ・幅を修正して正方形の画像が表示されるように修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -16,13 +16,24 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # デフォルト画像は700x700に収まるようリサイズ
-  process :resize_to_limit => [700, 700]
+  # 画像の上限をは500x500に設定
+  process :resize_to_limit => [500, 500]
   
-  # サムネイル画像
+  # サムネイルを生成する設定
   version :thumb do
-     process resize_to_fill: [100, 100]
+    process :resize_to_fill => [300, 300]
   end
+   
+  version :thumb100 do
+    process :resize_to_fill => [100, 100]
+  end
+ 
+  version :thumb30 do
+    process :resize_to_fill => [30, 30]
+  end
+  
+  # 保存形式をjpgに設定
+  process :convert => 'jpg'
   
   # 許可する画像の拡張子
   def extension_whitelist
@@ -32,6 +43,17 @@ class ImageUploader < CarrierWave::Uploader::Base
   # デフォルト画像を設定
   def default_url
     "default.jpg"
+  end
+
+  # 拡張子が同じでないとGIFをJPGとかにコンバートできない
+  def filename
+    super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
+  end
+
+  # ファイル名を日付にすると編集時のデータ受け渡し等で不具合が出るため、
+  # ファイル名をランダムで一位にする
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
   end
   
   protected
