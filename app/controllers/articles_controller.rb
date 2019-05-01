59e5a923a0fcd0126ec122f65f9b5c41988c4a61
takahisa <s.takahisa_0903@docomo.ne.jp>
2019-05-01 09:31:46 +0000

    #22 change: 仕様変更
    store_nameカラムの値と
    Stationカラムの値を組み合わせた検索機能を設定

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,6 +10,7 @@ class ArticlesController < ApplicationController
   # post /articles
   def create
     @article = Article.new(article_params)
+    # logger.debug @city.errors.inspect
     @article.user = current_user
     if @article.save
     # logger.debug @article.errors.inspect 
@@ -22,11 +23,20 @@ class ArticlesController < ApplicationController
   # get /article/:id
   def show
     @article = Article.find(params[:id])
+    @city = City.find(@article.city_id) #登録されたcity_idを元にCityオブジェクトを検索
     @comment = Comment.new #コメント投稿フォーム用に空のオブジェクトを作成
   end
   
   # get /articles
   def index
+    # 複数の単語を複数のカラムに対して検索
+    words = params[:q].delete(:store_name) if params[:q].present?
+    if words.present?
+      params[:q][:groupings] = []
+      words.split(/[ 　]/).each_with_index do |word, i| #全角空白と半角空白で切って、単語ごとに処理
+        params[:q][:groupings][i] = { store_name_or_station_cont: word }
+      end
+    end
     # 検索オブジェクト
     @search = Article.ransack(params[:q])
     # 検索結果
@@ -35,7 +45,8 @@ class ArticlesController < ApplicationController
   
   # get /artcles/:id/edit
   def edit
-    @article = Article.find(params[:id]) 
+    @article = Article.find(params[:id])
+    @city = City.find(@article.city_id) #登録されたcity_idを元にCityオブジェクトを検索
   end
   
   # patch /artcles/:id
@@ -57,9 +68,9 @@ class ArticlesController < ApplicationController
   #ストロングパラメータで指定したキーの値を受け取ることを許可
   private
     def article_params
-      params.require(:article).permit(:store_name, :adress, :access, :area,
-    :station, :access, :wifi, :plug, :business_hours,
-    :regular_holiday, :phone, :url, :image, :image_cache, :remove_image)
+      params.require(:article).permit(:store_name, :adress, :access,:city_id,
+      :station, :access, :wifi, :plug, :business_hours,
+      :regular_holiday, :phone, :url, :image, :image_cache, :remove_image)
     end
     
     def admin_user
