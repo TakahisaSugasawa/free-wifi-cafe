be8060ba97391fb1db7198575b05fa25d0cf56e5
takahisa <s.takahisa_0903@docomo.ne.jp>
2019-05-01 02:01:53 +0000

    #22 cityモデルとarticleモデルにリレーションを設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
+  belongs_to :city
   has_many :comments, dependent: :destroy
   validates :store_name, presence: true
   mount_uploader :image, ImageUploader #Articleモデルとアップローダーを紐づける    
