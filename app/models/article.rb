cafb9186bf407cc5c27224c84db6d2d0fd09da68
takahisa <s.takahisa_0903@docomo.ne.jp>
2019-05-03 05:51:13 +0000

    #15 add:favorite_userメソッドを追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,12 @@ class Article < ApplicationRecord
   belongs_to :user
   belongs_to :city
   has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   validates :store_name, presence: true
   mount_uploader :image, ImageUploader #Articleモデルとアップローダーを紐づける    
+  
+  #ユーザーが既に記事に対してお気に入りしているかどうか確認する
+  def favorites_user(user_id)
+   favorites.find_by(user_id: user_id)
+  end
 end
