cb4e8e952d9d71c5ad67da2dc1e9701e6b842b26
takahisa <s.takahisa_0903@docomo.ne.jp>
2019-05-04 07:58:52 +0000

    #24 update:お気に入り関連のviewを修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,6 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+  
   def create
     @article = Article.find(params[:article_id])
     @favorite = Favorite.create(user_id: current_user.id, article_id: params[:article_id])
@@ -13,8 +15,6 @@ class FavoritesController < ApplicationController
   end
   
   def index
-    @favorites = Favorite.where(user_id: current_user.id).page(params[:page]).per(2)
-    # logger.debug @favorites
-    # @articles = Article.where(article_id: @favorites.article_id)
+    @favorites = Favorite.where(user_id: current_user.id).page(params[:page]).per(20)
   end
 end
