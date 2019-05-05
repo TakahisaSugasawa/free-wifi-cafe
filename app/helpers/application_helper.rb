f67a596dcf189c1554be1bee032a24d5484a5245
takahisa <s.takahisa_0903@docomo.ne.jp>
2019-05-05 03:34:55 +0000

    #2 update:ページ毎にタイトルが変わるように修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = "Free-Wifi-Cafe" # 自分のアプリ名を設定
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
