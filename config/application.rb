b22b239104f2e9b151e5cdd8ad3fd3595590bf4d
takahisa <s.takahisa_0903@docomo.ne.jp>
2019-05-05 11:51:39 +0000

    #25 revert:webコンソールの設定を取り消し

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,9 @@ module FreeWifiCafe
   class Application < Rails::Application
     config.i18n.default_locale = :ja
     
-    unless Rails.env.production?
-      config.web_console.whitelisted_ips = '106.171.53.249'
-    end
+    # unless Rails.env.production?
+    #   config.web_console.whitelisted_ips = '106.171.53.249'
+    # end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
