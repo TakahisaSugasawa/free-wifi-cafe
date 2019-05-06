0585a0ff2bf7b3a9ca9d9d30fa029061be53d127
takahisa <s.takahisa_0903@docomo.ne.jp>
2019-05-06 07:34:29 +0000

    #25 update:ファクトリのemailカラムにシーケンスを加えて一意になるように設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :user do
     username  "hogehoge"
     email "tester@example.com"
+    sequence(:email) {|n|"tester#{n}@example.com"}
     password  "test123456"
     password_confirmation "test123456"
   end
