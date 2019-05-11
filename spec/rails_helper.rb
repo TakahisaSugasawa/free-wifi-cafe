55e5d1e0e1d89ae25c497494f893f2ea1e01da47
takahisa <s.takahisa_0903@docomo.ne.jp>
2019-05-11 10:11:13 +0000

    #25 update:エラー時のみsave_and_open_pageメソッドが使われるように設定

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,14 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+
+require 'spec_helper'
+
 require 'rspec/rails'
-# Add additional requires below this line. Rails is not loaded until this point!
 
 require 'capybara/rspec'
 
@@ -56,4 +59,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.after do |example|
+    if example.metadata[:type] == :feature and example.exception.present? and example.metadata[:open_on_error] == true
+      save_and_open_page
+    end
+  end
 end
