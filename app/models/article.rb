95f894099d6f5b2fabfbadad3e8614c5df432978
takahisa <s.takahisa_0903@docomo.ne.jp>
2019-05-18 09:19:50 +0000

    #13 add:Articleモデルにstore_nameカラムが一意になるように設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@ class Article < ApplicationRecord
   belongs_to :city
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  validates :store_name, presence: true
+  validates :store_name, presence: true,uniqueness: true
   mount_uploader :image, ImageUploader #Articleモデルとアップローダーを紐づける    
   
   #ユーザーが既に記事に対してお気に入りしているかどうか確認する
