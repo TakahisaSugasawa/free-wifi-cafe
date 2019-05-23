d30c918038b85604090136b84ad611b6af95e39f
takahisa <s.takahisa_0903@docomo.ne.jp>
2019-05-23 12:43:14 +0000

    #25 add:comment関連の単体テストを作成(仮)

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  describe 'コメント投稿機能'do
+    let(:user_admin)  { FactoryBot.create(:admin) }
+    
+    context '内容が入力されていない場合'do 
+      before do
+        @article = FactoryBot.create(:article,user: user_admin)
+        @comment = @article.comments.build(content: nil)
+        @comment.valid?
+      end
+      it 'コメント投稿に失敗する'do
+        expect(@comment.errors.full_messages).to include("内容 が入力されていません。")
+      end
+    end
+  end
+  
+end
