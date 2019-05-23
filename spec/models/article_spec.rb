28b40a53c37422cd18fea92788e6ff463f7ea8db
takahisa <s.takahisa_0903@docomo.ne.jp>
2019-05-23 12:41:46 +0000

    #25 add:article関連の単体テストを作成（仮）

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+  describe '記事投稿機能'do
+    let(:user_admin)  { FactoryBot.create(:admin) }
+    
+    context '店名が入力されていない場合'do 
+      before do
+        @article = FactoryBot.build(:article,store_name: nil,user: user_admin)
+        @article.valid?
+      end
+      it '記事投稿に失敗する'do
+        expect(@article.errors.full_messages).to include("店名 が入力されていません。")
+      end
+    end
+    
+    context '店名が重複する場合'do 
+      before do
+        FactoryBot.create(:article,user: user_admin)
+        @article = FactoryBot.build(:article,user: user_admin)
+        @article.valid?
+      end
+      it '記事投稿に失敗する'do
+        expect(@article).not_to be_valid
+      end
+    end
+  end
+end
