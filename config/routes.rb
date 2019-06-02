7f55848f5a1ca22b7551366fd080681879125510
takahisa <s.takahisa_0903@docomo.ne.jp>
2019-06-02 02:35:54 +0000

    #3 update:マイページ更新の際、「現在のパスワード」を使用しないで更新できるように設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
-
+  # devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users, only: [:show]
 
   resources :articles do
